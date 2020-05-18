143c774bd0021175c852e5db3c26e5e89a171a23
Lino Maurer <lino@q-b.de>
2020-05-18 10:43:29 +0200

    routes added: index, show, new, create, delete + controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/destroy'
+  # Show all tasks -> first page
+  get 'tasks', to: 'tasks#index'
+  # Show individual task with more information // details
+  get 'tasks/:id', to: 'tasks#show'
+  # Initialize and create new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # Delete a selected task // ask for confirmation
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
